2805cfa91f4d26db27064ae79a2687aadb1bf424
victor <contatopessoal879@gmail.com>
2024-03-13 15:29:36 -0300

    feat: improving responsiveness in the location section

--- src/presentation/components/layout/main/location/location.jsx
+++ src/presentation/components/layout/main/location/location.jsx
@@ -11,53 +11,57 @@ export function Location() {
         <span className={styles.subtitle}>Nossa localização</span>
         <h1 className={styles.title}>Localização</h1>
         <div className={styles.box_location}>
-          <div className={styles.map}>
-            <Map />
+          <div className={styles.location}>
+            <div className={styles.map}>
+              <Map className={styles.map} />
+            </div>
           </div>
-          <div className={styles.box_contact}>
-            <div className={styles.time}>
-              <h3>Contato</h3>
-              <div>
-                <span>
-                  <ICON.BiTime className={styles.icon} />
-                </span>
-                <p>Quarta-feira a Domingo e Feriados: 8h às 18horas</p>
-              </div>
-              <div>
-                <span>
-                  <ICON.IoLocationOutline className={styles.icon} />
-                </span>
-                <p>Pesque & Pague Cirino, São Pedro - SP, 13520-000</p>
-              </div>
-              <div>
-                <span>
-                  <ICON.BiLogoWhatsapp className={styles.icon} />
-                </span>
-                <p>+55 19 99771-5391</p>
-              </div>
-              <div>
-                <span>
-                  <ICON.AiOutlineInstagram className={styles.icon} />
-                </span>
-                <a href="https://www.instagram.com/pesqueirocirino/?igshid=NzZlODBkYWE4Ng%3D%3D">
-                  <p>pesqueirocirino</p>
-                </a>
-              </div>
-              <div>
-                <span>
-                  <ICON.AiOutlineFacebook className={styles.icon} />
-                </span>
-                <a href="https://www.facebook.com/pesqueirocpesqueirocirino?mibextid=ZbWKwL-5391">
-                  Pesqueiro Cirino
-                </a>
-              </div>
-              <div>
-                <span>
-                  <ICON.AiOutlineFacebook className={styles.icon} />
-                </span>
-                <a href="https://www.facebook.com/pesqueirodocirino?mibextid=ZbWKwL">
-                  Pesqueiro Do Cirino
-                </a>
+          <div>
+            <div className={styles.box_contact}>
+              <div className={styles.time}>
+                <h3>Contato</h3>
+                <div>
+                  <span>
+                    <ICON.BiTime className={styles.icon} />
+                  </span>
+                  <p>Quarta-feira a Domingo e Feriados: 8h às 18horas</p>
+                </div>
+                <div>
+                  <span>
+                    <ICON.IoLocationOutline className={styles.icon} />
+                  </span>
+                  <p>Pesque & Pague Cirino, São Pedro - SP, 13520-000</p>
+                </div>
+                <div>
+                  <span>
+                    <ICON.BiLogoWhatsapp className={styles.icon} />
+                  </span>
+                  <p>+55 19 99771-5391</p>
+                </div>
+                <div>
+                  <span>
+                    <ICON.AiOutlineInstagram className={styles.icon} />
+                  </span>
+                  <a href="https://www.instagram.com/pesqueirocirino/?igshid=NzZlODBkYWE4Ng%3D%3D">
+                    <p>pesqueirocirino</p>
+                  </a>
+                </div>
+                <div>
+                  <span>
+                    <ICON.AiOutlineFacebook className={styles.icon} />
+                  </span>
+                  <a href="https://www.facebook.com/pesqueirocpesqueirocirino?mibextid=ZbWKwL-5391">
+                    Pesqueiro Cirino
+                  </a>
+                </div>
+                <div>
+                  <span>
+                    <ICON.AiOutlineFacebook className={styles.icon} />
+                  </span>
+                  <a href="https://www.facebook.com/pesqueirodocirino?mibextid=ZbWKwL">
+                    Pesqueiro Do Cirino
+                  </a>
+                </div>
               </div>
             </div>
           </div>
